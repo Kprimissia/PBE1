4c44ee8c08157f65524c4efd4055ed7696ac933b
Kprimissia <159180445+Kprimissia@users.noreply.github.com>
2024-08-27 12:30:42 -0300

    Add files via upload

---
+++ AT03/Aplicacao.java
@@ -0,0 +1,37 @@
+package prjPokemon;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		Pokemon chamander = new Pokemon();
+		chamander.atributoNome = "charmander";
+		chamander.atributoTipo = "Fogo";
+		chamander.atributoNivel = 15;
+		chamander.atributoHp = 100;
+		
+		Pokemon squirtle = new Pokemon();
+		squirtle.atributoNome = "Squirtle";
+		squirtle.atributoTipo = "Água";
+		squirtle.atributoNivel = 14;
+		squirtle.atributoHp = 79;
+		
+		Pokemon pikachu = new Pokemon();
+		pikachu.atributoNome = "Pikachu";
+		pikachu.atributoTipo = "Elétrico";
+		pikachu.atributoNivel = 16;
+		pikachu.atributoHp = 97;
+	
+		Pokemon Pidgey = new Pokemon();
+		Pidgey.atributoNome = "Pidgey";
+		Pidgey.atributoTipo = "Voador";
+		Pidgey.atributoNivel = 10;
+		Pidgey.atributoHp = 99;
+		
+		Pokemon rattata = new Pokemon();
+		rattata.atributoNome = "rattata";
+		rattata.atributoTipo = "Normal";
+		rattata.atributoNivel = 17;
+		rattata.atributoHp = 96;
+	}
+
+}
