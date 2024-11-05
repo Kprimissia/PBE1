6a19448626419c61b8dfda763ceba6ed3a8ed5f2
Kprimissia <159180445+Kprimissia@users.noreply.github.com>
2024-11-05 12:27:39 -0300

    Delete AT10/br.com.bibliotecasenai.principal directory

--- AT10/br.com.bibliotecasenai.principal/src/main/java/br/com/bibliotecasenai/repositories/LivroRepositorie.java
+++
@@ -1,8 +0,0 @@
-package br.com.bibliotecasenai.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.bibliotecasenai.principal.entities.Livro;
-
-public interface LivroRepositorie extends JpaRepository<Livro,Long> {
-
-}
