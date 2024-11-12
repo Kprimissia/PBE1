a4f78fc8e2acb07203638edfc97510b11312feeb
Kprimissia <159180445+Kprimissia@users.noreply.github.com>
2024-11-12 13:33:15 -0300

    Add files via upload

---
+++ AT13/mercado/src/main/java/com/senaidev/mercado/entities/Produto.java
@@ -0,0 +1,59 @@
+package com.senaidev.mercado.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column
+	private long id_produto;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private double preco;
+
+	public Produto(long id_produto, String nome, double preco) {
+		super();
+		this.id_produto = id_produto;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public long getId_produto() {
+		return id_produto;
+	}
+
+	public void setId_produto(long id_produto) {
+		this.id_produto = id_produto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+}
